46f24e7cceedf46b0d8b942aa2e49beecc964ddf
Nataliya Pogodina <n.pogodina@gmail.com>
2020-01-24 08:54:01 -0800

    Added two types of flash messages and styled them.

--- routes/index.js
+++ routes/index.js
@@ -33,7 +33,7 @@ router.post("/register", function(req, res){
 
 // Show login form
 router.get("/login", function(req, res){
-    res.render("login", {message: req.flash("error")});
+    res.render("login");
 });
 
 // Sign the user in
@@ -47,6 +47,7 @@ router.post("/login", passport.authenticate("local",
 // Logout route
 router.get("/logout", function(req, res){
     req.logout();
+    req.flash("success", "Logged you out.");
     res.redirect("/cafes");
 });
 
