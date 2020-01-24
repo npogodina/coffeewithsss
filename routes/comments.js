76dfa29711cb1014f815ef5efd1d0ca49885ee08
Nataliya Pogodina <n.pogodina@gmail.com>
2020-01-23 19:02:13 -0800

    Added functionality to remove comments.

--- routes/comments.js
+++ routes/comments.js
@@ -68,6 +68,17 @@ router.put("/:comment_id", function(req, res){
     });
 });
 
+// COOMENT DELETE ROUTE
+router.delete("/:comment_id", function(req, res){
+    Comment.findByIdAndRemove(req.params.comment_id, function(err){
+        if(err){
+            res.redirect("back");
+        } else {
+            res.redirect("/cafes/" + req.params.id);
+        };
+    });
+});
+
 // Middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
